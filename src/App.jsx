986255300d0c68b50a21040c8121c8619110c618
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-11-08 12:49:18 +0100

    feat: create post list

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import blogPosts from "./data/blogPosts"; /* id, titolo, contenuto */
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="min-h-full">
+        <div className="container">
+          <h1 className="mt-16">Il mio blog</h1>
+
+          <div className="posts my-8">
+            <h2>Articoli</h2>
+            <ul>
+              {blogPosts.map((curPost) => (
+                <li key={curPost.id}>
+                  <h4>{curPost.titolo}</h4>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
